7451f67b5adbec6605586da102738326791cd9cf
Jordi Caballol <jcaballol94@gmail.com>
2022-08-24 22:51:27 +0100

    Refactor all the code for the sublime editor

---
+++ Editor/ProjectGeneration/CombinedProjectGenerator.cs
@@ -0,0 +1,31 @@
+
+namespace jCaballol94.IDE.Sublime
+{
+    internal class CombinedProjectGenerator : ProjectGeneratorBase
+    {
+        private SublimeProjectGenerator m_sublimeGen;
+        private SolutionGenerator m_solutionGen;
+
+        public override string SolutionPath => m_sublimeGen.SolutionPath;
+
+        public CombinedProjectGenerator() : base()
+        {
+            m_sublimeGen = new SublimeProjectGenerator(m_settings, m_tempFolder);
+            m_solutionGen = new SolutionGenerator(m_settings, m_tempFolder);
+        }
+
+        public override void Sync()
+        {
+            m_solutionGen.Sync();
+            m_sublimeGen.omniSharpSolution = m_solutionGen.SolutionPath;
+            m_sublimeGen.Sync();
+        }
+
+        public override void SyncIfNeeded(string[] affectedFiles, string[] importedFiles)
+        {
+            m_solutionGen.SyncIfNeeded(affectedFiles, importedFiles);
+            m_sublimeGen.omniSharpSolution = m_solutionGen.SolutionPath;
+            m_sublimeGen.SyncIfNeeded(affectedFiles, importedFiles);
+        }
+    }
+}
